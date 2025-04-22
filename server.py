f6babc314c85e5d726fe9640275de6d5aaaca33e
Ritsuru <user@USERnoMacBook-Pro.local>
2025-04-22 22:38:47 +0900

    first commit

---
+++ server.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI, WebSocket
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+clients = []
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    clients.append(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            for client in clients:
+                if client != websocket:
+                    await client.send_text(data)
+    except:
+        clients.remove(websocket)
