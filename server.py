ffe87165e7a74c260c56bea360266f38b0380362
Ritsuru <user@USERnoMacBook-Pro.local>
2025-04-24 01:04:25 +0900

    fix: use redis.asyncio

--- server.py
+++ server.py
@@ -8,6 +8,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
+redis_url = "redis://red-d04fkkk9c44c739jnp7g:6379"
 redis_client = redis.Redis.from_url(redis_url, decode_responses=True)
 
 @app.get("/")
